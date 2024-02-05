160c5feba02e8ed63ff91412e12c51791a83794d
5HT <maxim@synrc.com>
2024-02-05 21:53:43 +0200

    list.sh

--- list.sh
+++ list.sh
@@ -3,8 +3,9 @@
 export id=01707162568915302633
 export key=01707162568915283933
 
-curl -H "Auth: $key" -X GET "http://localhost:5010/subscription"
-curl -H "Auth: $key" -X GET "http://localhost:5010/component"
-curl -H "Auth: $key" -X GET "http://localhost:5010/site"
-curl -H "Auth: $key" -X GET "http://localhost:5010/incident"
-curl -H "Auth: $key" -X GET "http://localhost:5010/maintenance"
+curl -H "Auth: $key"   -X GET "http://localhost:5010/subscription"
+curl -H "Auth: $key"   -X GET "http://localhost:5010/component"
+curl -H "Auth: $key"   -X GET "http://localhost:5010/site"
+curl -H "Auth: $key"   -X GET "http://localhost:5010/incident"
+curl -H "Auth: $key"   -X GET "http://localhost:5010/maintenance"
+curl -H "Auth: secret" -X GET "http://localhost:5010/account"
