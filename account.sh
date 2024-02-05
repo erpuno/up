d479e4fecf7596efac310febc8520c448edf1bd5
5HT <maxim@synrc.com>
2024-02-05 19:41:17 +0200

    account all

---
+++ account.sh
@@ -0,0 +1,4 @@
+curl -H "Auth: secret" -X PUT "http://localhost:5010/account" -d @priv/account.json
+curl -H "Auth: secret" -X PUT "http://localhost:5010/account" -d @priv/account.json
+curl -H "Auth: secret" -X PUT "http://localhost:5010/account" -d @priv/account.json
+curl -H "Auth: secret" -X GET "http://localhost:5010/account"
---
+++ all.sh
@@ -0,0 +1,19 @@
+# copy id key from from result of account.sh
+
+export id=01707154556408094497
+export key=01707154556408001098
+
+curl -H "Auth: $key" -X PUT "http://localhost:5010/subscription/$id" -d @priv/subscription.json
+curl -H "Auth: $key" -X GET "http://localhost:5010/subscription"
+curl -H "Auth: $key" -X PUT "http://localhost:5010/component/$id" -d @priv/component.json
+curl -H "Auth: $key" -X PUT "http://localhost:5010/component/$id" -d @priv/component.json
+curl -H "Auth: $key" -X GET "http://localhost:5010/component"
+curl -H "Auth: $key" -X PUT "http://localhost:5010/site/$id" -d @priv/site.json
+curl -H "Auth: $key" -X PUT "http://localhost:5010/site/$id" -d @priv/site.json
+curl -H "Auth: $key" -X GET "http://localhost:5010/site"
+curl -H "Auth: $key" -X PUT "http://localhost:5010/incident/$id" -d @priv/incident.json
+curl -H "Auth: $key" -X PUT "http://localhost:5010/incident/$id" -d @priv/incident.json
+curl -H "Auth: $key" -X GET "http://localhost:5010/incident"
+curl -H "Auth: $key" -X PUT "http://localhost:5010/maintenance/$id" -d @priv/maintenance.json
+curl -H "Auth: $key" -X PUT "http://localhost:5010/maintenance/$id" -d @priv/maintenance.json
+curl -H "Auth: $key" -X GET "http://localhost:5010/maintenance"
